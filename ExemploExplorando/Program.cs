650df4a565a661e407df9e67f347fda1f5438120
Joao <joaovictorcordeiro00@gmail.com>
2023-12-12 10:05:55 -0300

    validações GET e SET

--- ExemploExplorando/Program.cs
+++ ExemploExplorando/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa();
+Pessoa p2 = new Pessoa();
+
+p1.Nome = "João";
+p1.Idade = 22;
+
+p2.Nome = "";
+p2.Idade = 23;
+
+p1.Apresentar();
+//p2.Apresentar();
