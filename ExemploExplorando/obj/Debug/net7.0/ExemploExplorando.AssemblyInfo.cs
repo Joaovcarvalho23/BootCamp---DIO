95b6988754d824a091bd8be7bc79fb6a3ff779e8
Joao <joaovictorcordeiro00@gmail.com>
2023-12-13 09:59:25 -0300

    ajustando numeração

--- ExemploExplorando/obj/Debug/net7.0/ExemploExplorando.AssemblyInfo.cs
+++ ExemploExplorando/obj/Debug/net7.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52c03af9cef90f0383d1fcb465157052b04d67d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a076b0a56beeadb4e71c4204810e048c2ab67661")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
