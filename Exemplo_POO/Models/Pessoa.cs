e8736707940892427a10d619cfaee4b95b88c8eb
João Victor Carvalho <joaovictorcordeiro00@gmail.com>
2023-12-17 00:14:59 -0300

    Herança na prática

--- Exemplo_POO/Models/Pessoa.cs
+++ Exemplo_POO/Models/Pessoa.cs
@@ -9,6 +9,7 @@ namespace Exemplo_POO.Models
     {
         public string Nome { get; set; }
         public int Idade { get; set; }
+        public string Email { get; set; }
 
 
         public void Apresentar()
--- Exemplo_POO/Program.cs
+++ Exemplo_POO/Program.cs
@@ -1,10 +1,36 @@
 ﻿using Exemplo_POO.Models;
 
 
-ContaCorrente c1 = new ContaCorrente(123, 1000);
-c1.ExibirSaldo();
-c1.Sacar(5000);
-c1.ExibirSaldo();
+Aluno a1 = new Aluno();
+a1.Nome = "João Victor Carvalho";
+a1.Idade = 22;
+a1.Apresentar();
+a1.Nota = 8.7;
+a1.Email = "joao@gmail.com";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ContaCorrente c1 = new ContaCorrente(123, 1000);
+// c1.ExibirSaldo();
+// c1.Sacar(5000);
+// c1.ExibirSaldo();
 
 
 
