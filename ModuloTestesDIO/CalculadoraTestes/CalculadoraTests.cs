0ae890f9759afe62d29a7f404955f438396c6ee4
João Victor Carvalho <joaovictorcordeiro00@gmail.com>
2023-12-18 18:16:06 -0300

    Implementando o teste unitário

--- ModuloTestesDIO/CalculadoraTestes/CalculadoraTests.cs
+++ ModuloTestesDIO/CalculadoraTestes/CalculadoraTests.cs
@@ -17,6 +17,16 @@ public class CalculadoraTests
     {
         //Arrange - Serve para podermos montar o cenário de teste. Iremos somar 5 com 10. Então, devemos disponibilizar esse números para ele e preparar todo o cenário.
         //Act - Já temos o cenário, agora falta agir! Vamos chamar ele para saber o que ele deve fazer. Se já preparamos o 5 e o 10, vamos executar a ação 'Somar'
-        //Assert - Serve para validar tudo isso acima possui o resultado esperado, que nesse caso é 15. 
+        //Assert - Serve para validar tudo isso acima possui o resultado esperado, que nesse caso é 15.
+
+        //Arrange
+        int n1 = 5;
+        int n2 = 10;
+
+        //Act
+        int resultado = _calc.Somar(n1, n2);
+
+        //Assert
+        Assert.Equal(15, resultado);
     }
 }
