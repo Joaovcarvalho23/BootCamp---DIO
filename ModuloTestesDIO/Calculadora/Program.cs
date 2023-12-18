886b1a467910f55d6b51221ebb04a254bd676b3f
João Victor Carvalho <joaovictorcordeiro00@gmail.com>
2023-12-18 17:52:47 -0300

    implementando classe Calculadora

--- ModuloTestesDIO/Calculadora/Program.cs
+++ ModuloTestesDIO/Calculadora/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Calculadora.Services;
+
+CalculadoraImplementacao calculadora = new CalculadoraImplementacao();
+
+int n1 = 5;
+int n2 = 10;
+
+Console.WriteLine($"{n1} + {n2} = {calculadora.Somar(n1, n2)}");
