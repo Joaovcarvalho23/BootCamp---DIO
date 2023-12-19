ebad0d33aaa0fa0c99fc18c02a0e0a0634710f89
Joao <joaovictorcordeiro00@gmail.com>
2023-12-19 09:22:44 -0300

    Utilizando propriedades e métodos

--- PraticaPOODesafiosCodigo/Program.cs
+++ PraticaPOODesafiosCodigo/Program.cs
@@ -5,15 +5,74 @@ class Program
 {
     static void Main()
     {
-        string nome = Console.ReadLine();
-        int idade;
-        while (!int.TryParse(Console.ReadLine(), out idade))
+        string[] velocidades = Console.ReadLine().Split(' ');
+
+        int vmin;
+        while (!int.TryParse(velocidades[0], out vmin))
         {
-            Console.WriteLine("Idade inválida. Por favor, digite um valor numérico para a idade.");
+            Console.WriteLine("Velocidade mínima no formato inválido");
         }
 
-        Pessoa p = new Pessoa(nome, idade);
+        int vmax;
+        while (!int.TryParse(velocidades[1], out vmax))
+        {
+            Console.WriteLine("Velocidade máxima no formato inválido");
+        }
 
-        Console.WriteLine($"Nome: {p.nome}, Idade: {p.idade}");
+        Robo robot = new Robo(vmin, vmax);
+
+        string comandos = Console.ReadLine();
+
+        foreach (var item in comandos)
+        {
+            if(item == 'A')
+            {
+                robot.Acelerar();
+            }
+
+            if(item == 'D')
+            {
+                robot.Desacelerar();
+            }
+        }
+
+        Console.WriteLine(robot.VelocidadeAtual);
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // string nome = Console.ReadLine();
+        // int idade;
+        // while (!int.TryParse(Console.ReadLine(), out idade))
+        // {
+        //     Console.WriteLine("Idade inválida. Por favor, digite um valor numérico para a idade.");
+        // }
+
+        // Pessoa p = new Pessoa(nome, idade);
+
+        // Console.WriteLine($"Nome: {p.nome}, Idade: {p.idade}");
     }
 }
