68458cb93c68bfa04c3fbdb359d331c4755f490d
Joao <joaovictorcordeiro00@gmail.com>
2024-04-23 12:48:16 -0300

    desafios de codigo basicos

--- ExemploFundamentos/ExemploFundamentos.NETAtual/Program.cs
+++ ExemploFundamentos/ExemploFundamentos.NETAtual/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Lista para armazenar os itens
+        List<string> itens = new List<string>();
+
+        // TODO: Solicite os itens ao usuário
+        for (int i = 0; i <3; i++)
+        {
+            string item = Console.ReadLine();
+            itens.Add(item);
+        }
+
+        // Exibe a lista de itens
+        Console.WriteLine("Lista de itens:");
+        foreach (string item in itens)
+        {
+            Console.WriteLine($"- {item}");
+        }
+    }
+}
+
+// using System;
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         int saldoTotal = int.Parse(Console.ReadLine());
+//         int valorSaque = int.Parse(Console.ReadLine());
+        
+//         string resultado = RealizarSaque(saldoTotal, valorSaque);
+//         Console.WriteLine(resultado);
+//     }
+
+//     static string RealizarSaque(int saldoTotal, int valorSaque)
+//     {
+//         if (saldoTotal >= valorSaque)
+//         {
+//             int novoSaldo = saldoTotal - valorSaque;
+//             return $"Saque realizado com sucesso! Novo saldo: {novoSaldo}";
+//         }
+//         else
+//         {
+//             return "Saldo insuficiente. Saque não realizado!";
+//         }
+//     }
+// }
