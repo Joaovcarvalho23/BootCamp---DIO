0ec8b950ad314572818bd7b02c1e90a0ae5c6b33
João Victor Carvalho <joaovictorcordeiro00@gmail.com>
2024-04-26 18:18:32 -0300

    fazendo desafio de projeto

--- ExemploExplorando/Program.cs
+++ ExemploExplorando/Program.cs
@@ -2,16 +2,126 @@
 using Newtonsoft.Json;
 using System.Globalization;
 
-string conteudoArquivo = File.ReadAllText("Arquivos/vendas.json");
+// string conteudoArquivo = File.ReadAllText("Arquivos/vendas.json");
 
-List<Venda> listaVenda = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
+// List<Venda> listaVenda = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
 
-foreach (Venda itemVenda in listaVenda)
-{
-    Console.WriteLine($"Id: {itemVenda.Id}, Produto: {itemVenda.Produto}, Preço: {itemVenda.Preco}, "+
-                      $"Data da venda: {itemVenda.DataDaVenda.ToString("dd/MM/yyyy")} às {itemVenda.DataDaVenda.ToString("HH:mm")}");    
-}
+// foreach (Venda itemVenda in listaVenda)
+// {
+//     Console.WriteLine($"Id: {itemVenda.Id}, Produto: {itemVenda.Produto}, Preço: {itemVenda.Preco}, "+
+//                       $"Data da venda: {itemVenda.DataDaVenda.ToString("dd/MM/yyyy")} às {itemVenda.DataDaVenda.ToString("HH:mm")}");    
+// }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Program
+// {
+//     static string VerificarForcaSenha(string senha)
+//     {
+//         int comprimentoMinimo = 8;
+//         bool temLetraMaiuscula = false;
+//         bool temLetraMinuscula = false;
+//         bool temNumero = false;
+//         bool temCaractereEspecial = false;
+
+//         // TODO: Verifique o comprimento da senha
+//         if(senha.Length < 8)
+//            return "Sua senha e muito curta. Recomenda-se no minimo 8 caracteres.";
+        
+
+//         // Verificando se a senha contém letras maiúsculas e minúsculas
+//         temLetraMaiuscula = senha.Any(char.IsUpper);
+//         temLetraMinuscula = senha.Any(char.IsLower);
+
+//         // Verificando se a senha contém sequências comuns
+//         string[] sequenciasComuns = { "123456", "abcdef" };
+//         if (sequenciasComuns.Any(s => senha.Contains(s)))
+//             return "Sua senha contem uma sequencia comum. Tente uma senha mais complexa.";
+
+//         // Verificando se a senha contém palavras comuns
+//         string[] palavrasComuns = { "password", "123456", "qwerty" };
+//         if (palavrasComuns.Contains(senha))
+//             return "Sua senha e muito comum. Tente uma senha mais complexa.";
+
+//         // Verificando se a senha contém números e caracteres especiais
+//         temNumero = senha.Any(char.IsDigit);
+//         temCaractereEspecial = senha.Any(c => !char.IsLetterOrDigit(c));
+
+//         if (temLetraMinuscula && temLetraMaiuscula && temNumero && temCaractereEspecial)
+//             return "Sua senha atende aos requisitos de seguranca. Parabens!";
+//         else
+//             return "Sua senha nao atende aos requisitos de seguranca.";
+//     }
+
+//     static void Main(string[] args)
+//     {
+//         string senha = Console.ReadLine().Trim();
+
+//         string resultado = VerificarForcaSenha(senha);
+
+//         Console.WriteLine(resultado);
+//     }
+// }
 
 
 
