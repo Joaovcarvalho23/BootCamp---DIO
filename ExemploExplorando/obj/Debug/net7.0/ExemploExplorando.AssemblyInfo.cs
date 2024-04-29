7fb9a726bd67606ff87f38ddca643800f59ee4c0
Joao <joaovictorcordeiro00@gmail.com>
2024-04-29 12:39:42 -0300

    instalando pacote nuget do newtonsoft

--- ExemploExplorando/obj/Debug/net7.0/ExemploExplorando.AssemblyInfo.cs
+++ ExemploExplorando/obj/Debug/net7.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d03985f2ac88ecef96fe2227345158bed568866")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
